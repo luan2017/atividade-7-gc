5bdbc54d5d823080947d776c8ea6e5f298824e10
Luan-Pinho <luanpinhoo@gmail.com>
2024-09-20 19:44:33 -0400

    adicionando html pra visualizar o cálculo fatorial

--- fatorial.js
+++ fatorial.js
@@ -1,12 +1,17 @@
 function calcularFatorial(n) {
-    if (n < 0) return 'Fatorial não existe para números negativos';
-    if (n === 0 || n === 1) return 1;
-    let fatorial = 1;
-    for (let i = 2; i <= n; i++) {
-        fatorial *= i;
-    }
-    return fatorial;
+  if (n < 0) return "Fatorial não existe para números negativos";
+  if (n === 0 || n === 1) return 1;
+  let fatorial = 1;
+  for (let i = 2; i <= n; i++) {
+    fatorial *= i;
+  }
+  return fatorial;
 }
 
-const numero = 5;
-console.log(O fatorial de ${numero} é: ${calcularFatorial(numero)});
+document.getElementById("calcular").addEventListener("click", () => {
+  const numero = parseInt(document.getElementById("numero").value);
+  const resultado = calcularFatorial(numero);
+  document.getElementById(
+    "resultado"
+  ).innerText = `O fatorial de ${numero} é: ${resultado}`;
+});
